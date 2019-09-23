fd0b62070663c134298d412022cf21849560c0ed
nctbrtc <41221911+nctbrtc@users.noreply.github.com>
2019-09-23 08:24:32 -0400

    Create paste.user.js

---
+++ paste.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         EasyPaste
+// @namespace    NA
+// @version      0.1
+// @description  helps you to paste to multiple inputs!
+// @author       Mr. Barutcu
+// @match        */Gradebook/*
+// @grant        none
+
+
+// ==/UserScript==
+$(document).ready(function() {
+
+$('input').bind('paste', null, function(e){
+    $this = $(this);
+
+    setTimeout(function(){
+        var columns = $this.val().split(/\s+/);
+        $this.val(' ');
+        var i;
+
+        for(i=0; i < columns.length; i++){
+            var sayi = ((i*2)+4);
+            var formattedNumber = ("0" + sayi).slice(-2);
+
+            $('input[name="ctl00$c$gradeAssignment$rg1$ctl00$ctl'+formattedNumber+'$decimalTbxNumericValue"]').val(columns[i]);
+        }
+    }, 0);
+});
+
+    });
